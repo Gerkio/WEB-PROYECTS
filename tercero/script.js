6e2c0ba9e4cfc48065ef1448929b70e600d8fc6e
Gerkio <64035688+Gerkio@users.noreply.github.com>
2021-05-28 08:33:01 -0500

    main

--- tercero/script.js
+++ tercero/script.js
@@ -3,5 +3,7 @@ var nombre = "Sergio" , edad = 25;
 i(edad == 18){
     document.write("Bienvenido, Tienes 18");
 } else if (nombre = "arturo") {
-    document.write("Bienvenido" + nombre)
+    document.write("Bienvenido" + nombre);
+} else {
+    document.write("No tienes 18 años");
 }
