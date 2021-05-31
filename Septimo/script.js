de6040ce6a31a251f1fa58223ad651a9e891dfe5
Gerkio <64035688+Gerkio@users.noreply.github.com>
2021-05-31 16:54:35 -0500

    main

--- Septimo/script.js
+++ Septimo/script.js
@@ -1,6 +1,9 @@
 (function(){
 
     let actualizarHora = function(){
+
+            //creo y asigno las variables
+
             let fecha = new Date();
             let horas = fecha.getHours();
             let ampm;
@@ -10,7 +13,7 @@
             let mes = fecha.getMonth();
             let year = fecha.getFullYear();
 
-
+            //ponemos las variables creadas en el documento
             let pHoras = document.getElementById("horas");
             let pAmpm = document.getElementById("ampm");
             let pMins = document.getElementById("mins");
@@ -18,32 +21,33 @@
             let pDia = document.getElementById("dia");
             let pMes = document.getElementById("mes");
             let pYear = document.getElementById("year");
-
+            //se establece el dia de la semana
             let semana = ["domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
             pDiaSemana.textContent = semana [diaSemana];
-
+            
             pDia.textContent = dia;
-
+            //se establece el mes
             let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
             pMes.textContent = meses[mes];
-
+            //se establece el año
             pYear.textContent = year;
-
+            // se establece si es am o pm
             if (horas >= 12){
                 horas = horas - 12;
                 ampm = "PM";}
                 else {
                     ampm = "AM";
                 }
+            //se cambia las 00:00 por las 12:00
             if (horas == 0) {
                 horas = 12;
             };
-            
+            //se establece la hora
             pHoras.textContent = horas;
-
+            //se coloca un 0 como prefijo cuando solo hay un digito en los minutos
             if (mins < 10){mins = "0" + mins};
-
+            //se establecen los minutos
             pMins.textContent = mins;
             pAmpm.textContent = ampm;
     };
